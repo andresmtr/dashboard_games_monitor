3be398924d53e1630165002dbcbc2d3a447cb3ef
Andres Mauricio Triana Reina <andresmauriciotrianareina@Andress-MacBook-Pro.local>
2026-03-18 10:58:22 -0500

    actualizacion ip

--- game_tracker/settings.py
+++ game_tracker/settings.py
@@ -16,7 +16,7 @@ def env_list(name: str, default: str = "") -> list[str]:
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-change-me")
 DEBUG = env_bool("DJANGO_DEBUG", True)
 # ALLOWED_HOSTS = env_list("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost")
-ALLOWED_HOSTS = env_list("*")
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = env_list("DJANGO_CSRF_TRUSTED_ORIGINS", "")
 
 INSTALLED_APPS = [
